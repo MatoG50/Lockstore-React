1e73d0c5b73e2eb69e470f7d9684ab601330ee2a
MatoG50 <matog50@gmail.com>
2021-06-11 23:32:10 +0300

    error

--- src/EmployeeCard.js
+++ src/EmployeeCard.js
@@ -2,16 +2,22 @@ import { useSelector } from 'react-redux';
 import './EmployeeCard.css';
 
 const EmployeeCard = () => {
-  const employees = useSelector(state => state.displayEmployees);
-  console.log(employees);
-
-  return (
-    <div className='employee-card'>
-      <img id='emp-image' src='./resources/user.svg' alt='Employee'></img>
-      <p className='emp-name'>MGK</p>
-      <p className='emp-role'>Attendant</p>
-    </div>
+  const employees = useSelector(
+    state => state.displayEmployees.employees.users
   );
+  console.log(employees);
+  const employeeList = employees.map(emp => {
+    const { username, role } = emp;
+    return (
+      <div className='employee-card'>
+        <img id='emp-image' src='./resources/user.svg' alt='Employee'></img>
+        <p className='emp-name'>{username}</p>
+        <p className='emp-role'>{role}</p>
+      </div>
+    );
+  });
+
+  return <>{employeeList}</>;
 };
 
 export default EmployeeCard;
